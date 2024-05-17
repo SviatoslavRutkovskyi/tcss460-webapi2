8b58a21a9780065bf7c3556fd016ee415922692c
Slavik <rutkovsky2003@gmail.com>
2024-05-16 20:57:18 -0700

    Added tocken check, added & fixed tests

--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -2,13 +2,11 @@ import express, { Router } from 'express';
 
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
-import { messageRouter } from './message';
 import { bookRouter } from './books';
 
 const closedRoutes: Router = express.Router();
 
 closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
-closedRoutes.use('/message', messageRouter);
-closedRoutes.use('/books', bookRouter);
+closedRoutes.use('/books', checkToken, bookRouter);
 
 export { closedRoutes };
